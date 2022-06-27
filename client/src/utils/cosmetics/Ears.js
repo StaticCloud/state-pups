b8a73c416e299d85aa5a25c2234df160d650951b
StaticCloud <diegomarrs@gmail.com>
2022-06-26 20:23:30 -0700

    modularized ear cosmetics

---
+++ client/src/utils/cosmetics/Ears.js
@@ -0,0 +1,16 @@
+const ears = [
+    {
+        text: "Button",
+        file: ""
+    },
+    {
+        text: "Bat",
+        file: ""
+    },
+    {
+        text: "Erect",
+        file: ""
+    }
+]
+
+module.exports = { ears }
