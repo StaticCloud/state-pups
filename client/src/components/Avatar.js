f34c9c6835c05727a3857f658006255bb0134b26
StaticCloud <diegomarrs@gmail.com>
2022-07-07 21:53:00 -0700

    properly aligned images

--- client/src/components/Avatar.js
+++ client/src/components/Avatar.js
@@ -1,17 +1,17 @@
 import React from 'react';
+import './CSS/Avatar.css'
 
 import { useSelector } from 'react-redux';
 
 export default function Avatar() {
     const character = useSelector(state => state.character);
 
-
     return (
-        <div>
-            <img src={character.muzzle}></img>
-            <img src={character.ears}></img>
-            <img src={character.eyes}></img>
+        <div id="char-display">
             <img src={character.fur}></img>
+            <img src={character.eyes}></img>
+            <img src={character.ears}></img>
+            <img src={character.muzzle}></img>
         </div>
     );
 }
